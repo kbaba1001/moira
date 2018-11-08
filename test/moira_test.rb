c189f42c40d6484625b0ad618ebdae37bea8bc45
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-11-08 22:42:07 +0900

    setup test-unit

--- test/moira_test.rb
+++ test/moira_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class MoiraTest < Minitest::Test
+class MoiraTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::Moira::VERSION
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "moira"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'moira'
 
-require "minitest/autorun"
+require 'test/unit'
